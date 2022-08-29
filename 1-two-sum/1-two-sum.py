d5e184520758db579c1e89339c7143a0cd616845
Mir <95391693+Amiris17@users.noreply.github.com>
2022-08-29 16:15:36 -0400

    Time: 43 ms (96.17%), Space: 14.2 MB (86.10%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self,numbers,target):
+        hash_table={}
+        for i,n in enumerate(numbers):
+            complement=target-n
+            if complement in hash_table:
+                return [hash_table[complement],i]
+            hash_table[n]=i
+        return
