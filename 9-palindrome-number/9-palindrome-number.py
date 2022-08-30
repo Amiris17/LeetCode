8530097a3cb8ba841a29194b498634e87b9c3a97
Mir <95391693+Amiris17@users.noreply.github.com>
2022-08-30 04:49:25 -0400

    Time: 71 ms (76.63%), Space: 13.3 MB (63.51%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -5,7 +5,7 @@ class Solution(object):
        #beg=string[0]
         end=len(string)-1
         for i in range(len(string)):
-            if string[i]==string[end]:
+            if string[i]==string[end]: #if beginning is equal to the end decrement i else return false and if the else is not reached we return True
                 end-=1
             else:
                 return False
