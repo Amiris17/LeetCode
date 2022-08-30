ac64c3e4e3fae89d3c014af6a32227613eeadc1e
Mir <95391693+Amiris17@users.noreply.github.com>
2022-08-29 23:04:31 -0400

    Time: 26 ms (84.59%), Space: 13.5 MB (39.94%) - LeetHub

--- 65-valid-number/65-valid-number.py
+++ 65-valid-number/65-valid-number.py
@@ -1,12 +1,32 @@
 class Solution(object):
     def isNumber(self, s):
-        i = 0
-        sign = 1
-        dec_cnt=0 #track incorrect decimals
-
-        try :
-            float(s)
-        except:
-            return False
-        if 'nf' in s: return False
-        return True
+        dec,digit,symbol,e=False,False,False,False
+      
+            
+        
+        for char in s:
+            if char in "0123456789":
+                digit=True
+            elif char in "-+":
+                if symbol or digit or dec:
+                    return False
+                else:
+                    symbol=True
+            elif char in "eE":
+                if e or not digit:
+                    return False
+                else:
+                    e=True
+                    symbol=False
+                    dec=False
+                    digit=False
+            elif char in ".":
+                if dec or e:
+                    return False
+                else:
+                    dec=True
+            else:
+                return False
+        return digit
+            
+            
