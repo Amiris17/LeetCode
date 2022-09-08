6248ef8c2ead22096b4e1296fce8dcedb0597970
Mir <95391693+Amiris17@users.noreply.github.com>
2022-09-08 01:01:26 -0400

    Time: 4129 ms (19.26%), Space: 14.5 MB (39.85%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -2,17 +2,17 @@ class Solution(object):
     def pivotIndex(self, nums):
         if nums == "":
             return "-1"
-        total=sum(nums)
+
         left_sum = 0
-        length = len(nums) - 1
-        right_sum=0
-        pivot_index=0 # so we need to divert away from summing everything up and instead of looking at hte pivot index
-        place_holder=0
+        right_sum= 0
+
+
 
         for i in range(len(nums)):
-            right_sum=total-nums[i]-left_sum # basically what this is doing is subtracting from the entire total
+            left_sum += nums[i]
+            right_sum=sum(nums[i:])
             print(right_sum)
             if left_sum==right_sum:
                 return i
-            left_sum+=nums[i]
+
         return -1
