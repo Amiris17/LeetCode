7303d903d7c91da1d2a07cd16cc2847f3dfdb0c4
Mir <95391693+Amiris17@users.noreply.github.com>
2022-09-15 16:20:20 -0400

    Time: 22 ms (79.31%), Space: 13.7 MB (12.72%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -16,7 +16,10 @@ class Solution(object):
             if s[i]=="(" or s[i]=="{" or s[i]=="[": #open parentheses
                 stack_list.append(s[i])
                 
-            elif stack_list and dict1[s[i]]==stack_list[-1]:
+                #pushing opening down 
+                
+            elif stack_list and dict1[s[i]]==stack_list[-1]: # stack_list checks to make sure the stakc isnt empty and dict[s[i]] is just looking up to match the parentheses we also have the parenthese in reverse order due to the fact that stacwks are naturally lifo and we are mathcing opposites.
+
                 stack_list.pop()
             
             
