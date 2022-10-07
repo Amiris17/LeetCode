ee178315bddaa4e62b99c700421a298e82108aef
Mir <95391693+Amiris17@users.noreply.github.com>
2022-10-06 20:37:58 -0400

    Time: 59 ms (37.16%), Space: 13.8 MB (55.47%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def average(self, salary):
+        """
+        :type salary: List[int]
+        :rtype: float
+        """
+        
+        minimum=min(salary)
+        maximum=max(salary)
+        
+        
+        salary.remove(minimum)
+        salary.remove(maximum)
+        
+        
+        result=0
+        count=0
+        for i in salary:
+            result+=i
+            count+=1
+        return float(result/count)
