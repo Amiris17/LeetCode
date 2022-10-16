ec0d7ef0a4f65a7d0d3cdeb0749a59823433e17b
Mir <95391693+Amiris17@users.noreply.github.com>
2022-10-16 03:15:29 -0400

    Time: 22 ms (77.85%), Space: 13.4 MB (86.71%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -8,7 +8,7 @@ class Solution(object):
         i=0
         while i<len(s):
             if i+2 <len(s) and s[i+2]=="#":
-                val=int(s[i: i+2])
+                val=int(s[i: i+2])    #making the hash tag number a chracter and then just storing it at and adding it and getting its ascii value
                 resulting_str.append(chr(val+96))# its +96 cause character is from 1-9 lol...
                 i+=3
             else:
