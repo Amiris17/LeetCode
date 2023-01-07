e780783c28d8bd7e49353abb8fe7d02d8e833fdc
Mir <95391693+Amiris17@users.noreply.github.com>
2023-01-07 03:29:26 -0500

    Time: 16 ms (93.45%), Space: 13.7 MB (6.42%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -5,28 +5,25 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        #what if we convert to int 1 acts a 9 then convert to binary
-        res=""
-        carry=0
+        #what if we convert to int 1 acts a 9 then convert to binary WE USE XOR AND GATES HERE
+        #AND IS OUR CARRY
+        #XOR IS OUR ADDITION
         
         
-        a,b=a[::-1],b[::-1] # reverse the string because we want to start from rhs
+        #convert to binary 
         
-        #loop through the largest one 
-       
-        for i in range(max(len(a),len(b))):
-            digitA=ord(a[i])-ord("0") if i <len(a) else 0 #giving default value of 0 if we are out of bounds
-            digitB=ord(b[i])-ord("0") if i<len(b) else 0 #giving default value of 0 if we are out of bounds
-            
-            
-            total=digitA+digitB +carry
-            char=str(total %2)
-            
-            
-            res=char+res
-            carry = total // 2
+        a=int(a,2)
+        
+        b=int(b,2)
         
         
-        if carry:
-            res="1"+res
-        return res
+        while(b!=0):
+            a,b=(a^b),(a&b) <<1
+        return "{0:b}".format(a)
+        
+        
+        
+        
+        
+        
+      
