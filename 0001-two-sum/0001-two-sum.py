a6ba45fbb5773578a36ea6c03d464e0192e39ef2
Mir <95391693+Amiris17@users.noreply.github.com>
2023-01-09 13:01:17 -0500

    Time: 45 ms (88.75%), Space: 14.2 MB (90.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,30 +1,9 @@
 class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
-        
-        #we can brute force or use two pointer solution i dont remember hash table
-        
-        beg=0
-        end=len(nums)-1
-        res=0
-        
-        
-        
-        while beg<end:
-            if nums[beg]+nums[end]==target:
-                return beg,end
-            
-            end-=1
-            
-            if beg==end:
-                beg+=1
-                end=len(nums)-1
-                
-                
-            
-          
+    def twoSum(self,numbers,target):
+        hash_table={}
+        for i,n in enumerate(numbers):
+            complement=target-n
+            if complement in hash_table:
+                return [hash_table[complement],i]
+            hash_table[n]=i
+      
