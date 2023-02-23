6aaf848a86e1e83c1eed4d6106933e46bc3b022b
Mir <95391693+Amiris17@users.noreply.github.com>
2023-02-23 02:44:54 -0500

    Time: 462 ms (44.93%), Space: 27.4 MB (40.41%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -9,16 +9,15 @@ class Solution(object):
       
         #Two methods make a set and just compare statement with length.
         
-        #second create a hash and just false if its ever added
+        #second create a hash and just True if its ever added more than once
         
         dup_list={}
-        
-        
-        for i in nums:
-            if i not in dup_list:
-                dup_list[i]=i
+            
+            
+        for i in range(len(nums)):
+            if nums[i] not in dup_list:
+                dup_list[nums[i]]=1
             else:
                 return True
+                
         return False
-            
-            
