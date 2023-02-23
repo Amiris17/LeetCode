469f2a496cc794095f496b9d7d831a53f1021355
Mir <95391693+Amiris17@users.noreply.github.com>
2023-02-23 01:59:14 -0500

    Time: 41 ms (87.51%), Space: 14.3 MB (76.30%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,15 @@
 class Solution(object):
     def twoSum(self,numbers,target):
-        hash_table={}
-        for i,n in enumerate(numbers):
-            complement=target-n
-            if complement in hash_table:
-                return [hash_table[complement],i]
-            hash_table[n]=i
+        
+        
+        hash_={}
+        for i in range(len(numbers)):
+            if target-numbers[i] in hash_:
+                return [hash_[target-numbers[i]],i]
+            hash_[numbers[i]]=i
+        return [-1,-1]
+            
+            
+            
+            
       
