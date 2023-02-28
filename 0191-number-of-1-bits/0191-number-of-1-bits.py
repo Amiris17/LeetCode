df554145de8ecd61843ff57b68653eaa13d1cad9
Mir <95391693+Amiris17@users.noreply.github.com>
2023-02-28 16:17:24 -0500

    Time: 15 ms (85.39%), Space: 13.5 MB (41.12%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -4,26 +4,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
-        #
-        
-        bin_n=bin(n)
-        
-        str_n=str(bin_n)
-        
-        print(str_n)
-        
-        count=0
-        
-        for i in str_n:
-            print(i)
-            if i=="1":
-                count+=1
-                
-        return count
+        remainder_count=0
+        while n:
+            remainder_count+=n%2
+            n=n/2
+        return remainder_count
             
-            
-        #while we could convert it back to a 32 signed for our answer it doesnt matter
-        
-        
+       
         
