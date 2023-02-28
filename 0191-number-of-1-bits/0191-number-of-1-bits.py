66b2d1e72d5608e4df617852bce0b8c76f318e2b
Mir <95391693+Amiris17@users.noreply.github.com>
2023-02-28 16:21:26 -0500

    Time: 25 ms (29.97%), Space: 13.3 MB (91.33%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -6,8 +6,8 @@ class Solution(object):
         """
         remainder_count=0
         while n:
-            remainder_count+=n%2
-            n=n/2
+            remainder_count+=n%2 #0 wont matter we just get all 1's 
+            n=n>>1
         return remainder_count
             
        
