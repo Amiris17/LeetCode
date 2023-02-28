17b35078cb48cdb23ce385dd8e4f4f15ccbe4d2d
Mir <95391693+Amiris17@users.noreply.github.com>
2023-02-28 15:35:06 -0500

    Time: 36 ms (7.07%), Space: 13.4 MB (69.65%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        #
+        
+        bin_n=bin(n)
+        
+        str_n=str(bin_n)
+        
+        print(str_n)
+        
+        count=0
+        test=[]
+        for i in str_n:
+            print(i)
+            if i=="1":
+                count+=1
+                
+        return count
+            
+            
+        #while we could convert it back to a 32 signed for our answer it doesnt matter
+        
+        
+        
