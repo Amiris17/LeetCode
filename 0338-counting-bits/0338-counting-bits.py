119d40dd2b00f86666c6512a26aa36af7c4b3948
Mir <95391693+Amiris17@users.noreply.github.com>
2023-03-03 07:00:10 -0500

    Time: 55 ms (91.30%), Space: 17.2 MB (91.39%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -5,34 +5,13 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        temp_bin=0
-        
-        count=0
-        
-        list1=[]
         
         
-        # 1. we need to iterate through the range and find the correspondent binary number,then once we do this we can count the number of 1's. This sounds like a dictionary problem, were restricted to list however,and remember we need to count the number of 1's in this binary make it a list.
-        for i in range((n+1)):
+        
+        counter = [0]
+        for i in range(1, n+1):
             
-            temp_bin=bin(i)
-            temp_bin=temp_bin[2:] #here we have the binary number
-            count=0
-            
-            for j in str(temp_bin):
-        
-                if j =="1":
-                    count+=1
-            list1.append(count)
-            
-        
-                
-                
-        return list1
-    
-          
-    
-    
-    #time_complex=0(n^2)
-    
-    #memory o(n)
+            #remember in math that even numbers are 2n and odd numbers are 2n+1
+            #this is the same principle applied as rshifting is the same /2 and adding the i %2 is the +1 or 0
+            counter.append(counter[i >> 1] + i %2 )
+        return counter
