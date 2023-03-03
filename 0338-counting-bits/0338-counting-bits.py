1a8eaee225be929f01c9d1c5db4f823e224bc522
Mir <95391693+Amiris17@users.noreply.github.com>
2023-03-03 07:00:35 -0500

    Time: 59 ms (83.42%), Space: 17.4 MB (65.57%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -13,5 +13,5 @@ class Solution(object):
             
             #remember in math that even numbers are 2n and odd numbers are 2n+1
             #this is the same principle applied as rshifting is the same /2 and adding the i %2 is the +1 or 0
-            counter.append(counter[i >> 1] + i %2 )
+            counter.append(counter[i //2 ] + i %2 )
         return counter
