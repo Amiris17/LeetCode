5ace8798ec464c404456f066c58b998e15add99d
Mir <95391693+Amiris17@users.noreply.github.com>
2023-03-03 08:57:58 -0500

    Time: 21 ms (35.11%), Space: 13.5 MB (38.97%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -8,8 +8,15 @@ class Solution(object):
         
         ##convert to binary
         
-       
+        MAX_INT = 0x7FFFFFFF
+        MIN_INT = 0x80000000
+        MASK = 0x100000000
+
+        #this masks makes it so python has to respect the lengths of 32 BITS for maximum INT instead of dealin g with random values when overflowing
+        while (b!=0):
+            a,b=(a^b) %MASK,((a&b)<<1)%MASK
+        return a if a<=MAX_INT else ~((a%MIN_INT)^MAX_INT)
     
-        return sum([a,b])
+#this problem sucks and is terrible becasue evey language displays 32 bits differently and in java this solution would be efficient because of the way the compiler handles we understand hte logic yet have to make ane xception by using a mask 
         
         
